f0512bc662a3a4aab026f1a714ed6d3036afb4e0
GrandMasterJJ <wangjyun.lee@gmail.com>
2024-07-20 21:52:59 +0800

    The falling sand simulation could now simulate falling sand with a stream of sand falling from the top.

--- Falling_sand.py
+++ Falling_sand.py
@@ -4,9 +4,9 @@ from matplotlib.widgets import Button
 import numpy as np
 import random
 
-graph_col_size = 11
-graph_row_size = 11
-graph_middle_col = 6
+graph_col_size = 50
+graph_row_size = 50
+graph_middle_col = 25
 anim_running = False
 current_col = graph_middle_col
 t = 0
@@ -16,64 +16,49 @@ zvals = np.zeros((graph_row_size, graph_col_size))
 # init the first block
 #zvals[0, 5] = 1
 # initialize the first block
-zvals[0, 0] = 0.1
+zvals[0, graph_middle_col] = 1
 # zvals[10, 4] = 1
 # zvals[9, 5] = 1
 # zvals[10, 6] = 1
-
 def updatePlot(time):
     global zvals, img, current_col
+
+    # Initialize the falling grain at the top
+    zvals[0, graph_middle_col] = 1
     
-    if (current_col + 1 < graph_col_size and current_col - 1 > -1):
-        if (time < graph_col_size and zvals[time, graph_middle_col] != 1) :
-            zvals[time, graph_middle_col] = 1  # Set the current position to 1
-            if (time > 0):
-                zvals[time - 1, graph_middle_col] = 0  # Reset the previous position to 0 to create a falling effect.
-        
-        elif (zvals[time, current_col - 1] == 0 and zvals[time, current_col + 1] == 0):
-            previous_col = current_col
-            col_increment = random.choice([1, -1])
-            current_col = current_col + col_increment
-            zvals[time, current_col] = 1
-            print("current_col: ", current_col)
-
-            if (time > 0):
-                zvals[time - 1, previous_col] = 0  # Reset the previous position to 0
-        
-
-            print("time: ", time,"left and right has position")
-
-
-
-        elif (zvals[time, current_col + 1] == 0 and current_col + 1 < graph_col_size): ##
-            previous_col = current_col
-            col_increment = 1
-            current_col = current_col + col_increment
-            zvals[time, current_col] = 1
-            print("current_col: ", current_col)
-
-            if (time > 0):
-                zvals[time - 1, previous_col] = 0  # Reset the previous position to 0
-        
-            print("time: ", time,"right has position")
+    # Create a temporary buffer for the new positions
+    new_zvals = zvals.copy()
     
-        elif (zvals[time, current_col - 1] == 0):
-            previous_col = current_col
-            col_increment = -1
-            current_col = current_col + col_increment
-            zvals[time, current_col] = 1
-            print("current_col: ", current_col)
+    # Move existing sand grains down
+    for row in range(graph_row_size - 1, 0, -1):
+        for col in range(graph_col_size):
+            if zvals[row, col] == 0 and zvals[row-1, col] == 1:
+                new_zvals[row, col] = 1
+                new_zvals[row-1, col] = 0
+            elif zvals[row, col] == 1 and row < graph_row_size - 1:
+                if zvals[row+1, col] == 0:
+                    new_zvals[row+1, col] = 1
+                    new_zvals[row, col] = 0
+                elif (zvals[row+1, col-1]) == 0 and (zvals[row+1, col+1]) == 0:
+                    col_incriment = random.choice([1, -1])
+                    new_zvals[row + 1, col - col_incriment] = 1
+                    new_zvals[row, col] = 0
+                elif col > 0 and zvals[row+1, col-1] == 0:
+                    new_zvals[row+1, col-1] = 1
+                    new_zvals[row, col] = 0
+                elif col < graph_col_size - 1 and zvals[row+1, col+1] == 0:
+                    new_zvals[row+1, col+1] = 1
+                    new_zvals[row, col] = 0
 
-            if (time > 0):
-                zvals[time - 1, previous_col] = 0  # Reset the previous position to 0
-        
-            print("time: ", time,"left has position")
-    
+    # Update the main grid with the new positions
+    zvals = new_zvals
 
-
-    img.set_array(zvals)  # Update the image data
-    plt.draw()  # Redraw the current figure
+    # Update the image data
+    img.set_array(zvals)
+    plt.draw()
     print("time:", time)
+
+
 
 
 # make a color map of fixed colors
@@ -92,20 +77,13 @@ ax.tick_params(which='both', bottom=False, left=False, labelbottom=False, labell
 
 ax.grid(which='minor', color='k', linestyle='-', linewidth=0.5)
 
-# Button callbacks
 def start(event):
-    global anim_running, t, current_col
+    global anim_running, t
     anim_running = True
     while anim_running:
-        plt.pause(0.01)  # Adjust the pause duration as needed
+        plt.pause(0.001)  # Adjust the pause duration as needed
         updatePlot(t)
         t += 1
-
-        if t == graph_col_size:
-            t = 0   # reset the time when a block reaches to the bottom 
-            # we have to reset the current_col here
-            current_col = graph_middle_col
-            #stop(None)  # Stop the animation when reaching the end, comment out this line if you want the falling sand to run forever. 
 
 def stop(event):
     global anim_running
